20f26930939ae12acf2d87ac09db4e9a2c2ac9e1
euggrie <eugeniagrieff@gmail.com>
2016-12-20 18:59:23 +0000

    Add index script and css

---
+++ js/script.js
@@ -0,0 +1,22 @@
+<!-- Google Map Location -->
+var myCenter = new google.maps.LatLng(-34.9066, -56.1992);
+
+function initialize() {
+var mapProp = {
+  center: myCenter,
+  zoom: 12,
+  scrollwheel: false,
+  draggable: false,
+  mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+
+var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+var marker = new google.maps.Marker({
+  position: myCenter,
+});
+
+marker.setMap(map);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
